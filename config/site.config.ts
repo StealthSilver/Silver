bdc825ed42331cd4b043f1febedb2747b041202d
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-09-01 13:16:50 +0530

    font and layout setup including metadata

---
+++ config/site.config.ts
@@ -0,0 +1,8 @@
+export const SITE = {
+  name: "Silver Portfolio",
+  description:
+    "Full-stack software engineer specializing in modern web development.",
+  url: "https://example.com", // 👈 change this once when you get your domain
+  ogImage: "/og-image.png",
+  twitterHandle: "@yoursocialhandle",
+};
