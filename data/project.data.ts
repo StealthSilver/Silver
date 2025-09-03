7c5bcd3cfda5ce8b8b63d03ab39392bad0410ef8
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-09-03 23:31:25 +0530

    project section

--- data/project.data.ts
+++ data/project.data.ts
@@ -1,77 +1,122 @@
 export const PROJECTS = [
   {
-    title: "Sgrids",
-    description: "Landing page for Renewables SAP application",
-    image: "/projects/sgrids.png",
-    live: "https://sgrids.vercel.app/",
-    github: "https://github.com/StealthSilver/sgrids-lp",
-    figma: "https://figma.com/file/your-figma-link",
+    title: "MeshSpire",
+    description: "A peer to peer learning app",
+    image: "/projects/sgrids.png", //
+    live: "https://mesh-spire-landing.vercel.app/",
+    github: "https://github.com/StealthSilver/MeshSpire-lp",
+    figma:
+      "https://www.figma.com/design/fkprR8ZCbDezgeWweqLc6X/MS-landing?node-id=0-1&t=gscDeXAEkqGmTJlM-1",
   },
   {
-    title: "Sgrids",
-    description: "Landing page for Renewables SAP application",
-    image: "/projects/sgrids.png",
-    live: "https://sgrids.vercel.app/",
-    github: "https://github.com/StealthSilver/sgrids-lp",
-    figma: "https://figma.com/file/your-figma-link",
+    title: "RiffinityAI",
+    description: "An AI companion application",
+    image: "/projects/sgrids.png", //
+    live: "https://riffinity-l18s.vercel.app/",
+    github: "https://github.com/StealthSilver/Riffinity",
+    figma: "https://figma.com/file/your-figma-link", //
   },
-  {
-    title: "Sgrids",
-    description: "Landing page for Renewables SAP application",
-    image: "/projects/sgrids.png",
-    live: "https://sgrids.vercel.app/",
-    github: "https://github.com/StealthSilver/sgrids-lp",
-    figma: "https://figma.com/file/your-figma-link",
-  },
-
   {
     title: "Prominent",
     description: "Low Latency real time trading and investment application",
-    image: "/projects/prominent.png",
+    image: "/projects/sgrids.png", //
     live: "https://prominent-eight.vercel.app/",
     github: "https://github.com/StealthSilver/Prominent",
     figma:
-      "https://www.figma.com/design/UhdHOIha7xWSdqZwHjCc7M/Prominent?node-id=1-2&t=L4HUETpfoaiVYiw7-1",
+      "https://www.figma.com/design/UhdHOIha7xWSdqZwHjCc7M/Prominent?node-id=1-2&t=37UrjNWrYqGqM5ym-1",
   },
+  {
+    title: "ConnectRight",
+    description: "A video confrencing platforms",
+    image: "/projects/sgrids.png", //
+    live: "https://sgrids.vercel.app/", //
+    github: "https://github.com/StealthSilver/ConnectRight",
+    figma: "https://figma.com/file/your-figma-link", //
+  },
+  {
+    title: "BridgePay",
+    description: "A payment gateway application",
+    image: "/projects/sgrids.png",
+    live: "https://your-live-site.com",
+    github: "https://github.com/StealthSilver/BridgePay",
+    figma: "https://figma.com/file/your-figma-link",
+  },
+  {
+    title: "TravoPad",
+    description: "A colloborative drawing application",
+    image: "/projects/sgrids.png", //
+    live: "https://your-live-site.com", //
+    github: "https://github.com/StealthSilver/TavroPad",
+    figma: "https://figma.com/file/your-figma-link", //
+  },
+
   {
     title: "Sgrids",
     description: "Landing page for Renewables SAP application",
     image: "/projects/sgrids.png",
     live: "https://sgrids.vercel.app/",
     github: "https://github.com/StealthSilver/sgrids-lp",
-    figma: "https://figma.com/file/your-figma-link",
+    figma:
+      "https://www.figma.com/design/HTVIDAbVfEn8CzWVvqVoMs/Sgrids?node-id=0-1&t=SsSC4rk4RVRXyaHK-1",
   },
 
   {
     title: "DevOrbit",
     description: "Application for deploying and editing codebase",
-    image: "/projects/uptime.png",
-    live: "https://your-live-site.com",
-    github: "https://github.com/your-username/weather",
-    figma: "https://figma.com/file/your-figma-link",
+    image: "/projects/sgrids.png", //
+    live: "https://your-live-site.com", //
+    github: "https://github.com/StealthSilver/DevOrbit",
+    figma: "https://figma.com/file/your-figma-link", //
   },
   {
     title: "InfinityX",
     description: "Real time workflow mamagement applicaion",
-    image: "/projects/brilliant.png",
-    live: "https://your-live-site.com",
-    github: "https://github.com/your-username/tasks",
-    figma: "https://figma.com/file/your-figma-link",
+    image: "/projects/sgrids.png", //
+    live: "https://your-live-site.com", //
+    github: "https://github.com/your-username/tasks", //
+    figma: "https://figma.com/file/your-figma-link", //
+  },
+
+  {
+    title: "BinaryNetwork",
+    description: "A professional social media platform",
+    image: "/projects/sgrids.png", //
+    live: "https://your-live-site.com", //
+    github: "https://github.com/StealthSilver/BinaryNetwork",
+    figma: "https://figma.com/file/your-figma-link", //
   },
   {
-    title: "DevOrbit",
-    description: "Application for deploying and editing codebase",
-    image: "/projects/uptime.png",
-    live: "https://your-live-site.com",
-    github: "https://github.com/your-username/weather",
-    figma: "https://figma.com/file/your-figma-link",
+    title: "MindPalace",
+    description: "An organization and management application",
+    image: "/projects/sgrids.png", //
+    live: "https://your-live-site.com", //
+    github: "https://github.com/your-username/weather", //
+    figma: "https://figma.com/file/your-figma-link", //
   },
   {
-    title: "InfinityX",
-    description: "Real time workflow mamagement applicaion",
-    image: "/projects/brilliant.png",
+    title: "ConnectingDots",
+    description: "A course selling application with LMS",
+    image: "/projects/sgrids.png", //
     live: "https://your-live-site.com",
-    github: "https://github.com/your-username/tasks",
-    figma: "https://figma.com/file/your-figma-link",
+    github: "https://github.com/your-username/tasks", //
+    figma: "https://figma.com/file/your-figma-link", //
+  },
+  {
+    title: "Silver's Desk",
+    description: "A blog application",
+    image: "/projects/sgrids.png", //
+    live: "https://silver-desk.vercel.app/",
+    github: "https://github.com/StealthSilver/SilverDesk",
+    figma:
+      "https://www.figma.com/design/b00XL5lXg2hl2jUNbtePw9/Silver-s-Desk?node-id=0-1&t=MGYZvAoWbktNGfIE-1",
+  },
+  {
+    title: "Silver",
+    description: "A portfolio application",
+    image: "/projects/sgrids.png", //
+    live: "https://silver-jet.vercel.app/",
+    github: "https://github.com/StealthSilver/Silver",
+    figma:
+      "https://www.figma.com/design/vX7gVhAgVACysF1aFNcCtj/Portfolio?node-id=36-541&t=zlVUk0IN29zpPcVg-1",
   },
 ];
