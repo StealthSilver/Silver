8e6ca7c96bb0565b43a5e88414ffef46a68068a5
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-09-22 23:13:17 +0530

    revised links

--- data/project.data.ts
+++ data/project.data.ts
@@ -2,7 +2,7 @@ export const PROJECTS = [
   {
     title: "MeshSpire",
     description: "A peer to peer learning app",
-    image: "/projects/sgrids.png", //
+    image: "/projects/meshspire.png", //
     live: "https://mesh-spire-landing.vercel.app/",
     github: "https://github.com/StealthSilver/MeshSpire-lp",
     figma:
@@ -11,7 +11,7 @@ export const PROJECTS = [
   {
     title: "BinaryNetwork",
     description: "A professional social media platform",
-    image: "/projects/sgrids.png", //
+    image: "/projects/binarynetwork.png", //
     live: "https://your-live-site.com", //
     github: "https://github.com/StealthSilver/BinaryNetwork",
     figma: "https://figma.com/file/your-figma-link", //
@@ -19,7 +19,7 @@ export const PROJECTS = [
   {
     title: "TravoPad",
     description: "A colloborative drawing application",
-    image: "/projects/sgrids.png", //
+    image: "/projects/travopad.png", //
     live: "https://your-live-site.com", //
     github: "https://github.com/StealthSilver/TavroPad",
     figma: "https://figma.com/file/your-figma-link", //
@@ -27,7 +27,7 @@ export const PROJECTS = [
   {
     title: "RiffinityAI",
     description: "An AI companion application",
-    image: "/projects/sgrids.png", //
+    image: "/projects/riffinity.png", //
     live: "https://riffinity-l18s.vercel.app/",
     github: "https://github.com/StealthSilver/Riffinity",
     figma: "https://figma.com/file/your-figma-link", //
@@ -35,7 +35,7 @@ export const PROJECTS = [
   {
     title: "DevOrbit",
     description: "Application for deploying and editing codebase",
-    image: "/projects/sgrids.png", //
+    image: "/projects/devorbit.png", //
     live: "https://your-live-site.com", //
     github: "https://github.com/StealthSilver/DevOrbit",
     figma: "https://figma.com/file/your-figma-link", //
@@ -43,7 +43,7 @@ export const PROJECTS = [
   {
     title: "Prominent",
     description: "Low Latency real time trading and investment application",
-    image: "/projects/sgrids.png", //
+    image: "/projects/prominent.png", //
     live: "https://prominent-eight.vercel.app/",
     github: "https://github.com/StealthSilver/Prominent",
     figma:
@@ -52,7 +52,7 @@ export const PROJECTS = [
   {
     title: "ConnectRight",
     description: "A video confrencing platforms",
-    image: "/projects/sgrids.png", //
+    image: "/projects/connectright.png", //
     live: "https://sgrids.vercel.app/", //
     github: "https://github.com/StealthSilver/ConnectRight",
     figma: "https://figma.com/file/your-figma-link", //
@@ -60,7 +60,7 @@ export const PROJECTS = [
   {
     title: "BridgePay",
     description: "A payment gateway application",
-    image: "/projects/sgrids.png",
+    image: "/projects/bridgepay.png",
     live: "https://your-live-site.com",
     github: "https://github.com/StealthSilver/BridgePay",
     figma: "https://figma.com/file/your-figma-link",
@@ -69,7 +69,7 @@ export const PROJECTS = [
   {
     title: "Silver's Desk",
     description: "A blog application",
-    image: "/projects/sgrids.png", //
+    image: "/projects/blog.png", //
     live: "https://silver-desk.vercel.app/",
     github: "https://github.com/StealthSilver/SilverDesk",
     figma:
@@ -78,7 +78,7 @@ export const PROJECTS = [
   {
     title: "Silver",
     description: "A portfolio application",
-    image: "/projects/sgrids.png", //
+    image: "/projects/silver.png", //
     live: "https://silver-jet.vercel.app/",
     github: "https://github.com/StealthSilver/Silver",
     figma:
@@ -97,7 +97,7 @@ export const PROJECTS = [
   {
     title: "InfinityX",
     description: "Real time workflow mamagement applicaion",
-    image: "/projects/sgrids.png", //
+    image: "/projects/infinityx.png", //
     live: "https://your-live-site.com", //
     github: "https://github.com/your-username/tasks", //
     figma: "https://figma.com/file/your-figma-link", //
@@ -105,7 +105,7 @@ export const PROJECTS = [
   {
     title: "MindPalace",
     description: "An organization and management application",
-    image: "/projects/sgrids.png", //
+    image: "/projects/mindpalace.png", //
     live: "https://your-live-site.com", //
     github: "https://github.com/your-username/weather", //
     figma: "https://figma.com/file/your-figma-link", //
@@ -113,7 +113,7 @@ export const PROJECTS = [
   {
     title: "ConnectingDots",
     description: "A course selling application with LMS",
-    image: "/projects/sgrids.png", //
+    image: "/projects/connectiondots.png", //
     live: "https://your-live-site.com",
     github: "https://github.com/your-username/tasks", //
     figma: "https://figma.com/file/your-figma-link", //
