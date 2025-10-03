7182b3551d276b07ad02e09c914eba7964a9c36c
Silver <rajat.saraswat.0409@gmail.com>
2025-10-03 23:53:42 +0530

    Update Navbar.tsx

--- components/sections/Navbar.tsx
+++ components/sections/Navbar.tsx
@@ -23,6 +23,7 @@ export default function Navbar() {
     { name: "Education", href: "#education" },
     { name: "Projects", href: "#projects" },
     { name: "Certificates", href: "#certificates" },
+    {name: "Blogs", href:"https://patch-umbrella-c3a.notion.site/System-Design-27c5af667d9680f0b6d2c5baa513f7ad"},
   ];
 
   return (
